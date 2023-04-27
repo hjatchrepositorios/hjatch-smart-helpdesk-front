1eccbb50e7b6ea8f4ab3086b08e4a8b5b99f492a
desarrollo-hjatch <desarrollo.hjatch@gmail.com>
2023-04-27 18:59:48 -0500

    Tercero

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,39 @@
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { KeycloakAngularModule, KeycloakService } from 'keycloak-angular';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainComponent } from './components/main/main.component';
-
+function initializeKeycloack(keycloak:KeycloakService){
+  return () =>
+    keycloak.init({
+      config: {
+        url:'http://192.168.0.13:9999/auth',
+        realm: 'hjatch',
+        clientId: 'smarthelpdesk'
+      },
+      initOptions:{
+        onLoad: 'login-required',
+        flow: 'standard'
+      }
+    })
+}
 @NgModule({
-  declarations: [
-    AppComponent,
-    MainComponent
-  ],
+  declarations: [AppComponent, MainComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    KeycloakAngularModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initializeKeycloack,
+      multi: true,
+      deps: [KeycloakService],
+    },
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,4 +1,8 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { KeycloakService } from 'keycloak-angular';
+import { KeycloakProfile } from 'keycloak-js';
+
 
 @Component({
   selector: 'app-main',
@@ -6,5 +10,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./main.component.scss']
 })
 export class MainComponent {
+  userProfile: KeycloakProfile | null = null;
+  users: any[] = [];
+  constructor(private keycloakService: KeycloakService) {}
+  ngOnInit() {
+    this.keycloakService.loadUserProfile().then((userProfile: KeycloakProfile) => {
+      this.userProfile = userProfile;
+    }).catch((error) => {
+      console.error('Error al cargar el perfil del usuario:', error);
+    });
 
+  }
+
+  logout() {
+    this.keycloakService.logout();
+  }
 }
